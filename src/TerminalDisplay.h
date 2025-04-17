78a2876a564aa35b534c750ae2f58ee095ce80ad
Oliulla <mohammad.oliullah25@gmail.com>
2025-04-17 15:26:36 +0600

    feat: implement TerminalDisplay class for enhanced terminal UI; add cursor management and command handling

---
+++ src/TerminalDisplay.h
@@ -0,0 +1,31 @@
+#ifndef TERMINALDISPLAY_H
+#define TERMINALDISPLAY_H
+
+#include <QPlainTextEdit>
+#include <QTimer>
+
+class TerminalDisplay : public QPlainTextEdit {
+    Q_OBJECT
+    public:
+        explicit TerminalDisplay(QWidget *parent = nullptr);
+        void appendOutput(const QString &text);
+        void setPrompt(const QString &prompt);
+        void updateCursor();
+
+    signals:
+        void executeCommand(const QString &command);
+
+    protected:
+        void paintEvent(QPaintEvent *event) override;
+        void keyPressEvent(QKeyEvent *event) override;
+        void resizeEvent(QResizeEvent *event) override;
+
+    private:
+        QString m_prompt;
+        QString m_currentInput;
+        int m_cursorPos;
+        bool m_cursorVisible;
+        QTimer m_cursorTimer;
+};
+
+#endif // TERMINALDISPLAY_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
 #include <QApplication>
 #include "MainWindow.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication app(argc, argv);
-
-    MainWindow window;
-    window.show();
-
+    MainWindow w;
+    w.show();
     return app.exec();
 }
-// Compare this snippet from src/MainWindow.cpp:
-// #include "MainWindow.h"
-// #include <QPushButton>
-// #include <QVBoxLayout>
-// #include <QKeyEvent>
